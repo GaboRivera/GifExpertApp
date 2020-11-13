7249ae5974259083541a9bca3650052500075ca8
GaboRivera <gabo27rm@gmail.com>
2020-11-13 09:42:44 -0600

    Subo carpeta BUILD

---
+++ docs/precache-manifest.1b2454a8ae63fffe5103797286e91143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6965e076c45c61496e684ed34ab637bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "365a8662c5c957cd466e",
+    "url": "/static/css/main.4a7c8b33.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365a8662c5c957cd466e",
+    "url": "/static/js/main.b841dc23.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
