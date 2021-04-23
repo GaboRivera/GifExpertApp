3786311c170390847e67d52470e4f6fdfb4d718f
GaboRivera <gabo27rm@gmail.com>
2021-04-22 21:34:15 -0500

    elimino

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { useFetchGifs } from '../hooks/useFetchGifs'
-import { GifGridItem } from './GifGridItem'
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-    const { loading, data: images } = useFetchGifs(category)
-    return (
-        <>
-            <h3>{category}</h3>
-            {loading && <p>Cargando...</p>}
-            <div className='card-grid'>
-                {images.map((img) => (
-                    <GifGridItem key={img.id} {...img} />
-                ))}
-            </div>
-        </>
-    )
-}
+  const { loading, data: images } = useFetchGifs(category);
+  return (
+    <>
+      <h3>{category}</h3>
+      {loading && <p>Cargando...</p>}
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
